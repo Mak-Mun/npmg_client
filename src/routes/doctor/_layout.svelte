aefc257349cf1d50062069bc031f9e4166b7be18
didiermunezer38 <didiermunezer38@gmail.com>
2021-03-10 18:38:40 +0200

    add to doctor and ranger

---
+++ src/routes/doctor/_layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import AdminSidebar from '../../components/AdminSidebar.svelte';
+</script>
+<style>
+	main {
+		position: relative;
+		max-width: 56em;
+		background-color: white;
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
+
+<div class="min-h-screen flex flex-row bg-gray-100">
+<AdminSidebar/> 
+<main>
+	<slot></slot>
+</main>
+</div>
