7d30f76a07b1f804dfcd6a0c75ba3e1fe7ad749b
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-12 00:48:26 +0200

    prettier setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,29 @@
 module.exports = {
   future: {},
   purge: {
-    mode: 'all',
-    content: ['./src/**/*.svelte', './src/**/*.html'],
+    mode: "all",
+    content: ["./src/**/*.svelte", "./src/**/*.html"],
   },
   theme: {
     extend: {},
     fontFamily: {
-      'sans': ['Nunito Sans', 'Source Code Sans','sans-serif'],
-      'serif': ['ui-serif', 'Georgia'],
-      'mono': ['ui-monospace', 'SFMono-Regular'],
-      'display': ['Oswald'],
-      'body': ['Open Sans'],
-     },
-     textColor: {
-      motherGreen: '#017A4C',
-      secGreen: '#1FC14C',
-      successorColor:'#F0A500', 
-  },
+      sans: ["Nunito Sans", "Source Code Sans", "sans-serif"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+      display: ["Oswald"],
+      body: ["Open Sans"],
+    },
+    textColor: {
+      motherGreen: "#017A4C",
+      secGreen: "#1FC14C",
+      successorColor: "#F0A500",
+    },
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      primary: "#3490dc",
+      secondary: "#ffed4a",
+      danger: "#e3342f",
+    }),
   },
   variants: {},
   plugins: [],
