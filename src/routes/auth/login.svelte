efb9a50d2f1049d1690563fb98af8069c0093fd6
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-11 14:55:40 +0200

    added auth pages

---
+++ src/routes/auth/login.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+  <title>Login Page</title>
+</svelte:head>
+
+<section>
+  <h1 class="text-red-500">Login Page</h1>
+</section>
--- src/routes/login.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-  <title>Sapper project template</title>
-</svelte:head>
-
-<section>
-  <h1 class="text-red-500">Hello World</h1>
-</section>
