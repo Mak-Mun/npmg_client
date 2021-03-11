3cc7c509bf939c90e9b6f7850cd92921d000fbe0
didiermunezer38 <didiermunezer38@gmail.com>
2021-03-11 12:03:19 +0200

    top bar navigation

--- src/routes/admin/_layout.svelte
+++ src/routes/admin/_layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Sidebar from '../../components/Sidebar.svelte';
+	import TopNav from '../../components/TopNav.svelte';
 	import  List from '../../types/sidebar_items';
 	let list: List[] = [new List("/admin","Dashboard"),new List("/admin/gorillas","Gorillas"),new List("/admin/rangers","Rangers"),new List("/admin/gorillas","Doctors"),new List("/admin/kwitizina","Kwitizina"),new List("/admin/reports","Reports"),new List("/admin/settings","Settings")];
 </script>
@@ -13,7 +14,10 @@ main {
 
 <div class="min-h-screen flex flex-row bg-gray-100">
 <Sidebar list={list}/> 
-<main>
+<div class="flex flex-col w-screen">
+<TopNav/>
+<main class="float-left">
 	<slot></slot>
 </main>
+</div>
 </div>
---
+++ src/routes/admin/new_gorilla.svelte
@@ -0,0 +1,11 @@
+
+<style>
+
+</style>
+
+<svelte:head>
+	<title>NEW GORILLA</title>
+</svelte:head>
+<div class="container">
+<h1 class="font-bold">NEW GORILLA</h1>
+</div>
