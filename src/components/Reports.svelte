624ddff2809e22b2ae805c3e54d36b789f3578c5
didiermunezer38 <didiermunezer38@gmail.com>
2021-03-12 07:25:40 +0200

    data table

--- src/components/Reports.svelte
+++ src/components/Reports.svelte
@@ -2,18 +2,62 @@
 
 </script>
 <style>
-
+.text-green-500{
+    color: green;
+    background-color: white;
+    font-weight: 800;
+}
+.text-red-500{
+    color: red;
+    font-weight: 800;
+}
 </style>
 <div>
     <div class="px:2 m-2 md:px-5 md:m-5">
         <h1 class="text-xl font-semibold">DAILY REPORT VIEW</h1>
     </div>
 <div class="w-full md:flex">
-    <div class="w-full md:w-7/12 bg-white rounded border md:px-8 md:py-8 mr-3 h-screen">
+    <div class="w-full md:w-7/12 bg-white rounded border px-4 py-4 md:px-8 md:py-8 mr-3 h-screen">
         <div class="flex">
             <span class="pl-2 ">Name:</span>
             <span class="pl-2 ">CYIZERE</span>
         </div>
+        <table class="text-left w-full md:px-10">
+            <thead class="flex w-full">
+                <tr class="flex w-full mb-2 items-center justify-between">
+                    <th class="p-3 w-1/5">#</th>
+                    <th class="p-3 w-2/5">Body Part</th>
+                    <th class="p-3 w-2/5">Status</th>
+                </tr>
+            </thead>
+            <tbody class="flex flex-col items-center justify-between overflow-y-auto w-full -mt-3" style="max-height: 50vh;">
+                <tr class="flex w-full mb-2 items-center justify-between border">
+                    <td class="p-3 py-2 w-1/5">1</td>
+                    <td class="p-3 py-2 w-2/5">Heart</td>
+                    <td class="p-3 py-2 w-2/5 flex text-green-500"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" class="text-green-500 w-5 mt-1" fill="green"><path d="M21.856 10.303c.086.554.144 1.118.144 1.697 0 6.075-4.925 11-11 11s-11-4.925-11-11 4.925-11 11-11c2.347 0 4.518.741 6.304 1.993l-1.422 1.457c-1.408-.913-3.082-1.45-4.882-1.45-4.962 0-9 4.038-9 9s4.038 9 9 9c4.894 0 8.879-3.928 8.99-8.795l1.866-1.902zm-.952-8.136l-9.404 9.639-3.843-3.614-3.095 3.098 6.938 6.71 12.5-12.737-3.096-3.096z"/></svg><span class="px-2">NSA</span></td>
+                </tr>
+                <tr class="flex w-full mb-2 items-center justify-between border">
+                    <td class="p-3 py-2 w-1/5">2</td>
+                    <td class="p-3 py-2 w-2/5">Lungs</td>
+                    <td class="p-3 py-2 w-2/5 text-red-500 flex"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" class="text-green-500 w-5 mt-1" fill="red"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 16.538l-4.592-4.548 4.546-4.587-1.416-1.403-4.545 4.589-4.588-4.543-1.405 1.405 4.593 4.552-4.547 4.592 1.405 1.405 4.555-4.596 4.591 4.55 1.403-1.416z"/></svg><span class="px-2">NAS</span></td>
+                </tr>
+                <tr class="flex w-full mb-2 items-center justify-between border">
+                    <td class="p-3 py-2 w-1/5">3</td>
+                    <td class="p-3 py-2 w-2/5">Eyes</td>
+                    <td class="p-3 py-2 w-2/5 flex text-green-500"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" class="text-green-500 w-5 mt-1" fill="green"><path d="M21.856 10.303c.086.554.144 1.118.144 1.697 0 6.075-4.925 11-11 11s-11-4.925-11-11 4.925-11 11-11c2.347 0 4.518.741 6.304 1.993l-1.422 1.457c-1.408-.913-3.082-1.45-4.882-1.45-4.962 0-9 4.038-9 9s4.038 9 9 9c4.894 0 8.879-3.928 8.99-8.795l1.866-1.902zm-.952-8.136l-9.404 9.639-3.843-3.614-3.095 3.098 6.938 6.71 12.5-12.737-3.096-3.096z"/></svg><span class="px-2">NSA</span></td>
+                </tr>
+                <tr class="flex w-full mb-2 items-center justify-between border">
+                    <td class="p-3 py-2 w-1/5">4</td>
+                    <td class="p-3 py-2 w-2/5">Brain</td>
+                    <td class="p-3 py-2 w-2/5 text-red-500 flex"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" class="text-green-500 w-5 mt-1" fill="red"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 16.538l-4.592-4.548 4.546-4.587-1.416-1.403-4.545 4.589-4.588-4.543-1.405 1.405 4.593 4.552-4.547 4.592 1.405 1.405 4.555-4.596 4.591 4.55 1.403-1.416z"/></svg><span class="px-2">NAS</span></td>
+                </tr>
+                <tr class="flex w-full mb-2 items-center justify-between border">
+                    <td class="p-3 py-2 w-1/5">5</td>
+                    <td class="p-3 py-2 w-2/5">Legs</td>
+                    <td class="p-3 py-2 w-2/5 flex text-green-500"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" class="text-green-500 w-5 mt-1" fill="green"><path d="M21.856 10.303c.086.554.144 1.118.144 1.697 0 6.075-4.925 11-11 11s-11-4.925-11-11 4.925-11 11-11c2.347 0 4.518.741 6.304 1.993l-1.422 1.457c-1.408-.913-3.082-1.45-4.882-1.45-4.962 0-9 4.038-9 9s4.038 9 9 9c4.894 0 8.879-3.928 8.99-8.795l1.866-1.902zm-.952-8.136l-9.404 9.639-3.843-3.614-3.095 3.098 6.938 6.71 12.5-12.737-3.096-3.096z"/></svg><span class="px-2">NSA</span></td>
+                </tr>         
+            </tbody>
+        </table>
     </div>
     <div class="w-full md:w-5/12 bg-white rounded border px-4 py-3 ml-2 h-screen">
         <h2>Other recents</h2>
