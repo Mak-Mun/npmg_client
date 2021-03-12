9e398a0098d9e9f6d78ac901d6ae45dfe6363922
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-12 16:37:40 +0200

    setup urql

--- src/types/sidebar_items.ts
+++ src/types/sidebar_items.ts
@@ -1,8 +1,8 @@
-export default class List{
-    url: string;
-    name: string;
-    constructor(url:string,name:string) {
-        this.url = url;
-        this.name = name;
-     }
+export default class List {
+  url: string
+  name: string
+  constructor(url: string, name: string) {
+    this.url = url
+    this.name = name
+  }
 }
