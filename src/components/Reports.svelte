97aad5db89c0ee3baf8f9a1f3a2897d9d122968b
didiermunezer38 <didiermunezer38@gmail.com>
2021-03-12 06:31:26 +0200

    report view structure

---
+++ src/components/Reports.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+
+</script>
+<style>
+
+</style>
+<div>
+    <div class="px:2 m-2 md:px-5 md:m-5">
+        <h1 class="text-xl font-semibold">DAILY REPORT VIEW</h1>
+    </div>
+<div class="w-full md:flex">
+    <div class="w-full md:w-7/12 bg-white rounded border md:px-8 md:py-8 mr-3 h-screen">
+        <div class="flex">
+            <span class="pl-2 ">Name:</span>
+            <span class="pl-2 ">CYIZERE</span>
+        </div>
+    </div>
+    <div class="w-full md:w-5/12 bg-white rounded border px-4 py-3 ml-2 h-screen">
+        <h2>Other recents</h2>
+    </div>
+</div>
+</div>
