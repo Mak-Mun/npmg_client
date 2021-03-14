2ff55c3f053e9d11621baae24ffd6a4370440558
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-14 13:57:51 +0200

    fix

--- src/routes/auth/login.svelte
+++ src/routes/auth/login.svelte
@@ -3,13 +3,21 @@
 </svelte:head>
 
 <form>
-  <div class="flex flex-col mb-6">
+  <div>
     <!-- <label for="email" class="mb-2">Email </label> -->
-    <input
+    <!-- class="py-2 border rounded-md px-2 focus:ring-4 focus:ring-green-500 focus:ring-opacity-50  focus:outline-none focus:border-green-500" -->
+    <!-- <input
       type="email"
-      class="py-2 border rounded-md px-2 focus:ring-4 focus:ring-green-500 focus:ring-opacity-50  focus:outline-none focus:border-green-500"
+      class="form-input mt-1 block w-full"
       id="email"
       placeholder="Email"
+    /> -->
+    <input
+      class="mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 "
+      type="email"
+      required="true"
+      placeholder="Jane Doe"
+      aria-label="Full name"
     />
   </div>
 
@@ -17,16 +25,25 @@
     <!-- <label for="password">Password </label> -->
     <input
       type="password"
-      class="py-2 border rounded-md px-2 focus:ring-4 focus:ring-green-500 focus:ring-opacity-50  focus:outline-none focus:border-green-500"
+      class="py-2 border rounded-md px-2 focus:ring-4 focus:ring-green-500 focus:ring-opacity-50 focus:outline-none focus:border-green-500"
       id="password"
       placeholder="password"
     />
   </div>
 
   <div class="flex justify-between my-4">
-    <label for="remember">
-      <input type="checkbox" name="remberme" id="remember" /> Remember me?
-    </label>
+    <div>
+      <label for="remember" class="inline-flex items-center">
+        <input
+          type="checkbox"
+          class="border-gray-300 border-2 rounded text-green-600 focus:border-gray-300 focus:ring-black"
+          name="remberme"
+          id="remember"
+        />
+      </label>
+      Remember me?
+    </div>
+
     <a href="/auth/forgotpassword" class=" text-blue-500"> Forgot password? </a>
   </div>
 
