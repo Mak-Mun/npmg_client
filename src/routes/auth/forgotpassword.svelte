1f6943f39764ede53fd48775d35d9bc7bcd9b044
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-14 16:45:59 +0200

    reset password done

--- src/routes/auth/forgotpassword.svelte
+++ src/routes/auth/forgotpassword.svelte
@@ -3,16 +3,15 @@
 </svelte:head>
 
 <form class="grid grid-cols-1 gap-6">
-  <label for="email" class="block mb-5">
-    <span class="text-gray-700 text-xl mb-4">Your email here</span>
+  <label for="email" class="block">
+    <span class="text-gray-700">Email</span>
     <input
       type="email"
-      id="text"
-      placeholder="Boston Rockstack"
+      id="email"
+      placeholder="enter your email here"
       class="block bg-transparent focus:outline-none border-transparent focus:ring focus:border-green-500 w-full px-0.5 border-0 border-b-2  border-gray-300"
     />
   </label>
-
   <input
     type="submit"
     value="Reset your password"
