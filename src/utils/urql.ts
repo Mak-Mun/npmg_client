6ce5cb075c58f1bcc01d4c85317b6b83bb92a0ee
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-14 17:31:20 +0200

    setup urql

--- src/utils/urql.ts
+++ src/utils/urql.ts
@@ -1,13 +1,31 @@
-import { createClient } from "@urql/core"
+import {
+  cacheExchange,
+  createClient,
+  dedupExchange,
+  fetchExchange,
+} from "@urql/core"
+import { ssrExchange } from "@urql/svelte"
 
 const getToken = () => localStorage.getItem("AUTH_TOKEN")
 
+//TODO: (verite) setup ssr with sapper
+// export const ssr = ssrExchange({
+//   isClient: typeof window !== "undefined",
+//   initialState:
+//     typeof window === "undefined" ? undefined : window.__SAPPER__.preloaded,
+// })
+
 export const client = createClient({
   url: "https://npmg-server.herokuapp.com/graphql",
-  fetchOptions: () => {
-    const token = getToken()
-    return {
-      headers: { authorization: token ? `Bearer ${token}` : "" },
-    }
-  },
+  exchanges: [dedupExchange, cacheExchange, fetchExchange],
 })
+
+export default client
+
+//TODO: (verite) handle authentication
+//   fetchOptions: () => {
+//     const token = getToken()
+//     return {
+//       headers: { authorization: token ? `Bearer ${token}` : "" },
+//     }
+//   },
