22067eb44ba479bb1851d8b45e3bb4aeab68859d
didiermunezer38 <didiermunezer38@gmail.com>
2021-03-27 23:02:27 +0200

    starter to listing of namers

---
+++ src/routes/admin/kwitizina.svelte
@@ -0,0 +1,58 @@
+<div class="w-full flex flex-col">
+        <ul class="w-full flex items-center mx-auto md:mb-4">
+            <li class="w-1/2 text-center font-semibold cursor-pointer text-green-600">Ceremonies</li>
+            <li class="w-1/2 text-center font-semibold cursor-pointer">Namers</li>
+        </ul>
+        <div
+        class="bg-white rounded px-4 pt-3 pb-1 md:px-8 md:py-3 mr-3 md:flex md:w-11/12 longer"
+      >
+      <table class="w-full">
+          <thead>
+              <tr>
+                  <td></td>
+                  <td class="font-semibold text-gray-700">Namer</td>
+                  <td class="font-semibold text-gray-700">Contact</td>
+                  <td class="font-semibold text-gray-700">Gorilla named</td>
+                  <td class="font-semibold text-gray-700">More</td>
+              </tr>
+          </thead>
+          <tbody>
+              <tr class="border-b-4">
+                  <td class="py-4">1</td>
+                  <td class="py-4">Didier Munezero</td>
+                  <td class="py-4">0782915222</td>
+                  <td class="py-4">Umucyo</td>
+                  <td class="py-4">Former Pr Uganda</td>
+              </tr>
+              <tr class="border-b-4">
+                <td class="py-4">1</td>
+                <td class="py-4">Didier Munezero</td>
+                <td class="py-4">0782915222</td>
+                <td class="py-4">Umucyo</td>
+                <td class="py-4">Former Pr Uganda</td>
+            </tr>
+            <tr class="border-b-4">
+                <td class="py-4">1</td>
+                <td class="py-4">Didier Munezero</td>
+                <td class="py-4">0782915222</td>
+                <td class="py-4">Umucyo</td>
+                <td class="py-4">Former Pr Uganda</td>
+            </tr>
+            <tr class="border-b-4">
+                <td class="py-4">1</td>
+                <td class="py-4">Didier Munezero</td>
+                <td class="py-4">0782915222</td>
+                <td class="py-4">Umucyo</td>
+                <td class="py-4">Former Pr Uganda</td>
+            </tr>
+            <tr class="border-b-4">
+                <td class="py-4">1</td>
+                <td class="py-4">Didier Munezero</td>
+                <td class="py-4">0782915222</td>
+                <td class="py-4">Umucyo</td>
+                <td class="py-4">Former Pr Uganda</td>
+            </tr>
+          </tbody>
+      </table>
+    </div>
+</div>
