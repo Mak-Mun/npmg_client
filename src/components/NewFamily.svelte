a995a56dff16f722308baa4294f91e54bd3d00a2
didiermunezer38 <didiermunezer38@gmail.com>
2021-04-07 18:36:52 +0200

    model

--- src/components/NewFamily.svelte
+++ src/components/NewFamily.svelte
@@ -8,22 +8,15 @@
       <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
       <div class="inline-block align-bottom bg-white rounded text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full border-2 border-green-new" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
         <div class="m-3 mt-5">
-          <h1 class="text-xl font-semibold md:mb-5 uppercase">Email Invitations</h1>
-          <p class="md:ml-4">Send invitations, type user emails here</p>
-          <div class="w-3/4 md:ml-4">
-            <input type="text" placeholder="User email" class="rounded w-full border-2 border-green-new text-sm  py-2 px-3 mb-3 focus:outline-none">
-            <input type="text" placeholder="User email" class="rounded w-full border-2 border-green-new text-sm  py-2 px-3 mb-3 focus:outline-none">
-            <input type="text" placeholder="User email" class="rounded w-full border-2 border-green-new text-sm  py-2 px-3 mb-3 focus:outline-none">
-            <button type="button" class="text-green mx-3 underline focus:outline-none">+ Add another</button>
-          </div>
+          <h1 class="text-xl font-bold md:mb-5 uppercase">New Family</h1>
         </div>
         
         <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
           <button on:click={CloseModal} type="button" class="bg-logout w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
-            Cancel
+            Close
           </button>
           <button type="button" class="bg-green mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-            Invite
+            Add new
           </button>
         </div>
       </div>
