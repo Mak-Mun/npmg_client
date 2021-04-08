3ba3a7b113edf8d5102da06232668b4fb33fdcc5
didiermunezer38 <didiermunezer38@gmail.com>
2021-04-08 19:01:13 +0200

    header

--- src/components/Tasks.svelte
+++ src/components/Tasks.svelte
@@ -5,7 +5,8 @@
 <svelte:head>
   <title>Tasks</title>
 </svelte:head>
-<div>
+<div class="flex flex-col justify-center bg-white p-6">
+  <h1 class="font-bold w-full text-xl">Daily task assignment</h1>
 </div>
 
 <style>
