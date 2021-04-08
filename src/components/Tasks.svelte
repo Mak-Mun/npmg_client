d83716013c86ea6bb9d0649614313e1789e6feb9
didiermunezer38 <didiermunezer38@gmail.com>
2021-04-08 19:37:11 +0200

    this for starter

--- src/components/Tasks.svelte
+++ src/components/Tasks.svelte
@@ -7,7 +7,7 @@
 </svelte:head>
 <div class="flex flex-col justify-center bg-white p-6">
   <h1 class="font-bold w-full text-xl">Daily task assignment</h1>
-  <div class="w-10/12 flex flex-col sm:mx-4 md:mx-10 ">
+  <div class="w-10/12 flex flex-col sm:mx-4 md:mx-10 mt-4">
     <div class="w-full flex flex-col">
       <label for="date">Task Date</label>
       <input type="date" class="px-4 border-b-2 border-black focus:border-green-500 sm:w-5/6 md:w-4/12" name="task_date">
@@ -17,9 +17,9 @@
       <tr
         class="justify-between"
       >
-        <th class="p-4 text-center w-1/7">Numbering</th>
-        <th class="p-4 text-center w-1/7">Group name</th>
-        <th class="p-4 text-center w-1/7">Families and Gorillas</th>
+        <th class="p-2 text-center w-1/7">Numbering</th>
+        <th class="p-2 text-center w-1/7">Group name</th>
+        <th class="p-2 text-center w-1/7">Families and Gorillas</th>
       </tr>
     </thead>
     <tbody
@@ -28,11 +28,11 @@
         <tr
           class="mt-3 justify-between bg-white border-b-2 cursor-pointer"
         >
-          <td class="text-center p-4 w-1/7"
+          <td class="text-center p-2 w-1/7"
             >1</td
           >
-          <td class="text-center p-4 w-1/7">Horrible</td>
-          <td class="text-center p-4 w-1/7">Kwitonda</td>
+          <td class="text-center p-2 w-1/7">Horrible</td>
+          <td class="text-center p-2 w-1/7">Kwitonda</td>
         </tr>
     </tbody>
   </table>
