27af4ee3bbf3017ce7a2a2dcd016428d4e500704
didiermunezer38 <didiermunezer38@gmail.com>
2021-04-10 14:36:02 +0200

    data liket his at here

--- src/components/TaskView.svelte
+++ src/components/TaskView.svelte
@@ -8,7 +8,7 @@
 <div class="flex flex-col justify-center bg-white p-6">
     <h1 class="font-bold text-xl">Daily Task Assignment View</h1>
     <div class="flex w-full mt-2">
-        <div class="w-7/12 flex flex-col items-center mx-auto">
+        <div class="w-7/12 flex flex-col items-center mx-auto px-4">
         <div class="w-full flex mt-4 bg-primaryGreen text-white rounded-md py-6">
         <p class="font-semibold mx-4 text-xl">Family:</p>
         <p class="font-semibold mx-2 cursor-pointer text-xl">Kwitonda</p>
@@ -21,7 +21,7 @@
                     class="justify-between"
                   >
                     <th class="p-2 text-center w-1/7"></th>
-                    <th class="p-2 text-center w-1/7 text-green-500">Group</th>
+                    <th class="p-2 text-center w-1/7">Group</th>
                     <th class="p-2 text-center w-1/7">Gorilla Family</th>
                   </tr>
                 </thead>
@@ -29,7 +29,7 @@
                   class="limitedTable overflow-y-auto"
                 >
                     <tr
-                      class="mt-3 justify-between bg-white border-b-2 cursor-pointer border-green-200"
+                      class="mt-3 justify-between bg-white cursor-pointer border-b border-black"
                     >
                       <td class="text-center p-2 w-2/7"
                         >1</td
@@ -38,10 +38,28 @@
                       <td class="text-center p-2 w-2/7">Kwitonda</td>
                     </tr>
                     <tr
-                      class="mt-3 justify-between bg-white border-b-2 cursor-pointer"
+                      class="mt-3 justify-between bg-white cursor-pointer border-b border-black"
+                    >
+                      <td class="text-center p-2 w-2/7"
+                        >2</td
+                      >
+                      <td class="text-center p-2 w-2/7">Horrible</td>
+                      <td class="text-center p-2 w-2/7">Kwitonda</td>
+                    </tr>
+                    <tr
+                      class="mt-3 justify-between bg-white cursor-pointer border-b border-black"
+                    >
+                      <td class="text-center p-2 w-2/7"
+                        >3</td
+                      >
+                      <td class="text-center p-2 w-2/7">Horrible</td>
+                      <td class="text-center p-2 w-2/7">Kwitonda</td>
+                    </tr>
+                    <tr
+                      class="mt-3 justify-between bg-white cursor-pointer border-b border-black"
                     >
                       <td class="text-center p-2 w-1/7"
-                        >1</td
+                        >4</td
                       >
                       <td class="text-center p-2 w-1/7">Horrible</td>
                       <td class="text-center p-2 w-1/7">Kwitonda</td>
@@ -50,8 +68,59 @@
               </table>
            </div>
         </div>
-        <div class="w-5/12 flex flex-col items-center mx-auto">
+        <div class="w-5/12 flex flex-col items-center mx-auto px-4">
             <h2 class="font-semibold my-2 border-b-2 border-green-600 text-green-600">Kwitonda Members</h2>
+            <table class="w-full justify-between mt-4 mx-2">
+                <thead class="w-full">
+                  <tr
+                    class="justify-between"
+                  >
+                    <th class="p-2 text-center w-1/7"></th>
+                    <th class="p-2 text-center w-1/7">Gorilla</th>
+                    <th class="p-2 text-center w-1/7">Family</th>
+                  </tr>
+                </thead>
+                <tbody
+                  class="limitedTable overflow-y-auto"
+                >
+                    <tr
+                      class="mt-3 justify-between bg-white cursor-pointer border-b border-black"
+                    >
+                      <td class="text-center p-2 w-2/7"
+                        >1</td
+                      >
+                      <td class="text-center p-2 w-2/7">Amahoro</td>
+                      <td class="text-center p-2 w-2/7">Kwitonda</td>
+                    </tr>
+                    <tr
+                      class="mt-3 justify-between bg-white cursor-pointer border-b border-black"
+                    >
+                      <td class="text-center p-2 w-2/7"
+                        >2</td
+                      >
+                      <td class="text-center p-2 w-2/7">Munezero</td>
+                      <td class="text-center p-2 w-2/7">Kwitonda</td>
+                    </tr>
+                    <tr
+                      class="mt-3 justify-between bg-white cursor-pointer border-b border-black"
+                    >
+                      <td class="text-center p-2 w-2/7"
+                        >3</td
+                      >
+                      <td class="text-center p-2 w-2/7">Cyizere</td>
+                      <td class="text-center p-2 w-2/7">Kwitonda</td>
+                    </tr>
+                    <tr
+                      class="mt-3 justify-between bg-white cursor-pointer border-b border-black"
+                    >
+                      <td class="text-center p-2 w-1/7"
+                        >4</td
+                      >
+                      <td class="text-center p-2 w-1/7">Byishimo</td>
+                      <td class="text-center p-2 w-1/7">Kwitonda</td>
+                    </tr>
+                </tbody>
+              </table>
         </div>
     </div>
 </div>
