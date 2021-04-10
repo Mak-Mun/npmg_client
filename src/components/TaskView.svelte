344a04b61e90bb4bf0d42c2c10a6691914de6cea
didiermunezer38 <didiermunezer38@gmail.com>
2021-04-10 14:00:56 +0200

    view asigs

--- src/components/TaskView.svelte
+++ src/components/TaskView.svelte
@@ -8,8 +8,8 @@
 <div class="flex flex-col justify-center bg-white p-6">
     <h1 class="font-bold text-xl">Daily Task Assignment View</h1>
     <div class="flex w-full mt-4">
-        <div class="w-6/12 flex flex-col items-center mx-auto">
-        <div class="w-full">
+        <div class="w-7/12 flex flex-col items-center mx-auto">
+        <div class="w-full flex">
         <p class="font-semibold mx-4">Assigned Family:</p>
         <p class="font-semibold mx-2">Kwitonda</p>
            </div>
@@ -17,6 +17,9 @@
                <h1 class="font-semibold my-2">Full Task Assignment</h1>
                <table>Table of full assignment</table>
            </div>
+        </div>
+        <div class="w-5/12 flex flex-col items-center mx-auto">
+            <h2 class="font-semibold my-2 border-b-2 border-green-600 text-green-600">Kwitonda Members</h2>
         </div>
     </div>
 </div>
