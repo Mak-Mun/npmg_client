44f37023e866276c12730d3ba3568235c9e2d605
didiermunezer38 <didiermunezer38@gmail.com>
2021-04-14 20:25:00 +0200

    update

--- src/store/user.js
+++ src/store/user.js
@@ -3,7 +3,7 @@ import { writable } from "svelte/store"
 const USER = writable({
   fname: "Didier",
   lname: "Munezero",
-  year: "2015",
+  year: "2021",
   type: "ADMIN",
   photo:
     "https://avatars.githubusercontent.com/u/52195?s=460&u=08bcafa24337a298e1b874279fde515e2fb8f81d&v=4",
