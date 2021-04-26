cdb8107e7882874d92f675ae6acd208ccabea817
veritem <mugaboverite@gmail.com>
2021-04-26 21:45:13 +0200

    feat: setup tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,35 +1,33 @@
 module.exports = {
-	mode: "jit",
-	purge: [
-		"./src/**/*.{html,js,svelte,ts}",
-	],
+	mode: 'jit',
+	purge: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {},
 		fontFamily: {
-		  sans: ["Nunito", "Nunito Sans", "sans-serif"],
-		  sourceSans: ["Lato","Source Sans Pro", "sans-serif"],
-		  serif: ["ui-serif", "Georgia"],
-		  mono: ["ui-monospace", "SFMono-Regular"],
-		  display: ["Oswald"],
-		  body: ["Open Sans"],
+			sans: ['Nunito', 'Nunito Sans', 'sans-serif'],
+			sourceSans: ['Lato', 'Source Sans Pro', 'sans-serif'],
+			serif: ['ui-serif', 'Georgia'],
+			mono: ['ui-monospace', 'SFMono-Regular'],
+			display: ['Oswald'],
+			body: ['Open Sans']
 		},
 		textColor: (theme) => ({
-		  ...theme("colors"),
-		  motherGreen: "#00917C",
-		  secGreen: "#1FC14C",
-		  greenAccent: "#00917C",
-		  successorColor: "#F0A500",
+			...theme('colors'),
+			motherGreen: '#00917C',
+			secGreen: '#1FC14C',
+			greenAccent: '#00917C',
+			successorColor: '#F0A500'
 		}),
 		backgroundColor: (theme) => ({
-		  ...theme("colors"),
-		  primaryGreen: "#00917C",
-		  secondary: "#ffed4a",
-		  danger: "#e3342f",
-		  greenAccent: "#B7FFC2",
-		  blueAccent: "#86C5FF",
-		  redishAccent: "#86C5FF",
-		  primaryWhite: "#FCFDFF",
-		}),
+			...theme('colors'),
+			primaryGreen: '#00917C',
+			secondary: '#ffed4a',
+			danger: '#e3342f',
+			greenAccent: '#B7FFC2',
+			blueAccent: '#86C5FF',
+			redishAccent: '#86C5FF',
+			primaryWhite: '#FCFDFF'
+		})
 	},
-	plugins: [],
+	plugins: []
 };
