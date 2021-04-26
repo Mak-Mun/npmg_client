719b69fe2996aa7b28fcf1c6d86e0a1f9437c099
veritem <mugaboverite@gmail.com>
2021-04-26 19:12:57 +0200

    (feat): setup tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,31 @@ module.exports = {
 	],
 	theme: {
 		extend: {},
+		fontFamily: {
+		  sans: ["Nunito", "Nunito Sans", "sans-serif"],
+		  sourceSans: ["Lato","Source Sans Pro", "sans-serif"],
+		  serif: ["ui-serif", "Georgia"],
+		  mono: ["ui-monospace", "SFMono-Regular"],
+		  display: ["Oswald"],
+		  body: ["Open Sans"],
+		},
+		textColor: (theme) => ({
+		  ...theme("colors"),
+		  motherGreen: "#00917C",
+		  secGreen: "#1FC14C",
+		  greenAccent: "#00917C",
+		  successorColor: "#F0A500",
+		}),
+		backgroundColor: (theme) => ({
+		  ...theme("colors"),
+		  primaryGreen: "#00917C",
+		  secondary: "#ffed4a",
+		  danger: "#e3342f",
+		  greenAccent: "#B7FFC2",
+		  blueAccent: "#86C5FF",
+		  redishAccent: "#86C5FF",
+		  primaryWhite: "#FCFDFF",
+		}),
 	},
 	plugins: [],
 };
