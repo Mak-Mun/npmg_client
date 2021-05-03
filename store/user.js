8dfa4b7855efb7c887c561aef89ec667cbc001d1
didiermunezero <didiermunezer38@gmail.com>
2021-05-03 23:47:53 +0900

    fix: user

--- store/user.js
+++ store/user.js
@@ -1,19 +1,19 @@
-import { writable } from "svelte/store"
+import { writable } from 'svelte/store';
 
 const USER = writable({
-  fname: "Didier",
-  lname: "Munezero",
-  year: "2021",
-  type: "ADMIN",
-  photo:
-    "https://avatars.githubusercontent.com/u/52195?s=460&u=08bcafa24337a298e1b874279fde515e2fb8f81d&v=4",
-})
+	fname: 'Didier',
+	lname: 'Munezero',
+	year: '2021',
+	type: 'ADMIN',
+	photo:
+		'https://avatars.githubusercontent.com/u/52195?s=460&u=08bcafa24337a298e1b874279fde515e2fb8f81d&v=4'
+});
 
 const updateUser = (user) => {
-  USER.set(user)
-}
+	USER.set(user);
+};
 
 export default {
-  USER,
-  updateUser,
-}
+	USER,
+	updateUser
+};
