d224035b51c618b7f66787e2308b96bdd258d2a5
didiermunezero <didiermunezer38@gmail.com>
2021-05-03 23:12:50 +0900

    fix: redundancy

--- src/lib/Gorillas.svelte
+++ src/lib/Gorillas.svelte
@@ -1,74 +1,61 @@
 <script lang="ts">
-  import Search from './Search.svelte';
-  let photoUrl: string =
-    "https://avatars.githubusercontent.com/u/784056?s=64&v=4"
-  let integers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+	import Search from './Search.svelte';
+	let photoUrl = 'https://avatars.githubusercontent.com/u/784056?s=64&v=4';
+	let integers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 </script>
 
 <svelte:head>
-  <title>Gorillas</title>
+	<title>Gorillas</title>
 </svelte:head>
 <div>
-  <div class="w-full md:flex">
-    <div
-      class="w-full bg-white rounded pt-3 pb-1 md:px-8 md:py-3 mr-3 unlimited"
-    >
-      <div class="w-full md:flex mb-4 items-center justify-between">
-        <div class="md:w-4/12">
-        <h3 class="font-semibold px-3 py-1 text-xl">Gorillas listing</h3>
-      </div>
-        <div class="flex justify-between md:w-7/12">
-          <Search/>
-        <span class="px-3 py-1 font-semibold text-successorColor underline"
-          ><a href="/admin/families">View all families</a></span
-        >
-        <span class="bg-green px-3 py-1 font-semibold cursor-pointer rounded-sm"
-          ><a href="/admin/new_gorilla">Add New</a></span
-        >
-      </div>
-      </div>
-      <table class="w-full justify-between mt-0">
-        <thead class="w-full">
-          <tr
-            class="justify-between"
-          >
-            <th class="p-4 text-center w-1/7"></th>
-            <th class="p-4 text-center w-1/7">Name</th>
-            <th class="p-4 text-center w-1/7">Dob</th>
-            <th class="p-4 text-center w-1/7">Mother</th>
-            <th class="p-4 text-center w-1/7">Father</th>
-            <th class="p-4 text-center w-1/7">Namer</th>
-            <th class="p-4 text-center w-1/7">Family</th>
-          </tr>
-        </thead>
-        <tbody
-          class="limitedTable overflow-y-auto"
-        >
-          {#each integers as int}
-            <tr
-              class="mt-3 justify-between bg-white border-b-2 cursor-pointer"
-            >
-              <td class="text-center p-4 py-8 w-1/7">{int}</td>
-              <td class="text-center p-4 w-1/7"
-                ><div class="flex items-center">
-                  <div class="flex-shrink-0 w-8 h-8">
-                    <img
-                      class="w-full h-full rounded-full"
-                      src={photoUrl}
-                      alt=""
-                    />
-                  </div>
-                  <div class="ml-3">
-                    <p class="text-gray-900 whitespace-no-wrap">Byishimo</p>
-                  </div>
-                </div></td
-              >
-              <td class="text-center p-4 w-1/7"> 12/2018</td>
-              <td class="text-center p-4 w-1/7">Amahoro</td>
-              <td class="text-center p-4 w-1/7">Kagabo</td>
-              <td class="text-center p-4 w-1/7">Kwizigira</td>
-              <td class="text-center p-4 w-1/7">Gahinga</td>
-              <!-- <td class="p-4 w-1/7 flex">
+	<div class="w-full md:flex">
+		<div class="w-full bg-white rounded pt-3 pb-1 md:px-8 md:py-3 mr-3 unlimited">
+			<div class="w-full md:flex mb-4 items-center justify-between">
+				<div class="md:w-4/12">
+					<h3 class="font-semibold px-3 py-1 text-xl">Gorillas listing</h3>
+				</div>
+				<div class="flex justify-between md:w-7/12">
+					<Search />
+					<span class="px-3 py-1 font-semibold text-successorColor underline"
+						><a href="/admin/families">View all families</a></span
+					>
+					<span class="bg-green px-3 py-1 font-semibold cursor-pointer rounded-sm"
+						><a href="/admin/new_gorilla">Add New</a></span
+					>
+				</div>
+			</div>
+			<table class="w-full justify-between mt-0">
+				<thead class="w-full">
+					<tr class="justify-between">
+						<th class="p-4 text-center w-1/7" />
+						<th class="p-4 text-center w-1/7">Name</th>
+						<th class="p-4 text-center w-1/7">Dob</th>
+						<th class="p-4 text-center w-1/7">Mother</th>
+						<th class="p-4 text-center w-1/7">Father</th>
+						<th class="p-4 text-center w-1/7">Namer</th>
+						<th class="p-4 text-center w-1/7">Family</th>
+					</tr>
+				</thead>
+				<tbody class="limitedTable overflow-y-auto">
+					{#each integers as int}
+						<tr class="mt-3 justify-between bg-white border-b-2 cursor-pointer">
+							<td class="text-center p-4 py-8 w-1/7">{int}</td>
+							<td class="text-center p-4 w-1/7"
+								><div class="flex items-center">
+									<div class="flex-shrink-0 w-8 h-8">
+										<img class="w-full h-full rounded-full" src={photoUrl} alt="" />
+									</div>
+									<div class="ml-3">
+										<p class="text-gray-900 whitespace-no-wrap">Byishimo</p>
+									</div>
+								</div></td
+							>
+							<td class="text-center p-4 w-1/7"> 12/2018</td>
+							<td class="text-center p-4 w-1/7">Amahoro</td>
+							<td class="text-center p-4 w-1/7">Kagabo</td>
+							<td class="text-center p-4 w-1/7">Kwizigira</td>
+							<td class="text-center p-4 w-1/7">Gahinga</td>
+							<!-- <td class="p-4 w-1/7 flex">
                 <span class="cursor-pointer font-semibold pr-1 mx-1/2"
                   ><svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -94,47 +81,47 @@
                   ></span
                 >
               </td> -->
-            </tr>
-          {/each}
-        </tbody>
-      </table>
-    </div>
-  </div>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</div>
+	</div>
 </div>
 
 <style>
-  .limitedTable{
-    height: 60vh;
-  }
-  .unlimited {
-    height: 90vh;
-    overflow-y: auto;
-  }
-  @media only screen and (max-height: 550px) {
-    .unlimited {
-      height: fit-content;
-      width: fit-content;
-    }
-  }
-  @media only screen and (max-width: 600px) {
-    .unlimited {
-      width: fit-content;
-    }
-  }
-  .bg-green {
-    background-color: #00917c;
-    color: white;
-  }
-  div *::-webkit-scrollbar {
-    width: 4px;
-    display: none;
-  }
-  div *::-webkit-scrollbar-track {
-    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
-  }
+	.limitedTable {
+		height: 60vh;
+	}
+	.unlimited {
+		height: 90vh;
+		overflow-y: auto;
+	}
+	@media only screen and (max-height: 550px) {
+		.unlimited {
+			height: fit-content;
+			width: fit-content;
+		}
+	}
+	@media only screen and (max-width: 600px) {
+		.unlimited {
+			width: fit-content;
+		}
+	}
+	.bg-green {
+		background-color: #00917c;
+		color: white;
+	}
+	div *::-webkit-scrollbar {
+		width: 4px;
+		display: none;
+	}
+	div *::-webkit-scrollbar-track {
+		box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+	}
 
-  div::-webkit-scrollbar-thumb {
-    background-color: darkgrey;
-    outline: 1px solid slategrey;
-  }
+	div::-webkit-scrollbar-thumb {
+		background-color: darkgrey;
+		outline: 1px solid slategrey;
+	}
 </style>
--- src/lib/Naming.svelte
+++ src/lib/Naming.svelte
@@ -1,103 +1,102 @@
 <script>
-    import { Tabs, Tab, TabList, TabPanel } from 'svelte-tabs';
-    let integers = [1, 2, 3, 4, 5, 6, 7]
-    import Ceremonies from './Ceremonies.svelte'
-  </script> 
-  <svelte:head>
-    <title>Kwitizina</title>
-  </svelte:head>
-  <Tabs>
-    <TabList>
-      <Tab>Ceremonies</Tab>
-      <Tab>Namers</Tab>
-    </TabList>
-   
-    <TabPanel>
-     <Ceremonies/>
-    </TabPanel>
-   
-    <TabPanel>
-        <div
-        class="bg-white rounded px-4 pt-3 pb-1 md:px-8 md:py-3 mr-3 md:flex md:w-full unlimited"
-      >
-        <table class="w-full">
-            <thead class="w-full">
-                <tr class="justify-between">
-                    <th class="p-4 w-1/5"></th>
-                    <th class="p-4 w-1/5">Namer</th>
-                    <th class="p-4 w-1/5">Contacts</th>
-                    <th class="p-4 w-1/5">Gorillas named</th>
-                    <th class="p-4 w-1/5">More</th>
-                </tr>
-            </thead>
-            <tbody class="w-full">
-                {#each integers as int}
-                <tr class="mt-3 justify-between bg-white border-b-2 cursor-pointer ">
-                    <td class="text-center p-4 py-8 w-1/5">{int}</td>
-                    <td class="text-center p-4 w-1/5">Osita Iheme</td>
-                    <td class="text-center p-4 w-1/5 text-sm">didiermunezero@gmail.com 078324452343</td>
-                    <td class="text-center w-1/5 text-sm">Cyizere</td>
-                    <td class="text-center p-4 w-1/5">This is special to this one</td>
-                </tr>
-                {/each}
-                </tbody>
-        </table>
-    </div>
-    </TabPanel>
-  </Tabs>
+	import { Tabs, Tab, TabList, TabPanel } from 'svelte-tabs';
+	let integers = [1, 2, 3, 4, 5, 6, 7];
+	import Ceremonies from './Ceremonies.svelte';
+</script>
+
+<svelte:head>
+	<title>Kwitizina</title>
+</svelte:head>
+<Tabs>
+	<TabList>
+		<Tab>Ceremonies</Tab>
+		<Tab>Namers</Tab>
+	</TabList>
+
+	<TabPanel>
+		<Ceremonies />
+	</TabPanel>
+
+	<TabPanel>
+		<div class="bg-white rounded px-4 pt-3 pb-1 md:px-8 md:py-3 mr-3 md:flex md:w-full unlimited">
+			<table class="w-full">
+				<thead class="w-full">
+					<tr class="justify-between">
+						<th class="p-4 w-1/5" />
+						<th class="p-4 w-1/5">Namer</th>
+						<th class="p-4 w-1/5">Contacts</th>
+						<th class="p-4 w-1/5">Gorillas named</th>
+						<th class="p-4 w-1/5">More</th>
+					</tr>
+				</thead>
+				<tbody class="w-full">
+					{#each integers as int}
+						<tr class="mt-3 justify-between bg-white border-b-2 cursor-pointer ">
+							<td class="text-center p-4 py-8 w-1/5">{int}</td>
+							<td class="text-center p-4 w-1/5">Osita Iheme</td>
+							<td class="text-center p-4 w-1/5 text-sm">didiermunezero@gmail.com 078324452343</td>
+							<td class="text-center w-1/5 text-sm">Cyizere</td>
+							<td class="text-center p-4 w-1/5">This is special to this one</td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</div>
+	</TabPanel>
+</Tabs>
 
 <style>
-    .unlimited {
-      height: 75vh;
-      overflow-y: auto;
-    }
-    div *::-webkit-scrollbar {
-      width: 4px;
-      display: none;
-    }
-    div *::-webkit-scrollbar-track {
-      box-shadow: inset 0 0 6px rgba(197, 119, 119, 0.3);
-    }
-  
-    div::-webkit-scrollbar-thumb {
-      background-color: darkgrey;
-      outline: 1px solid slategrey;
-    }
-    @media only screen and (max-height: 550px) {
-      .unlimited {
-        height: fit-content;
-        width: fit-content;
-      }
-    }
-    @media only screen and (max-width: 600px) {
-      .unlimited {
-        height: fit-content;
-        width: fit-content;
-      }
-    }
-    :global(.svelte-tabs li.svelte-tabs__selected){
-        border-bottom: 4px solid #00917C;
-        color: #00917C;
-        font-weight: 800;
-    }
+	.unlimited {
+		height: 75vh;
+		overflow-y: auto;
+	}
+	div *::-webkit-scrollbar {
+		width: 4px;
+		display: none;
+	}
+	div *::-webkit-scrollbar-track {
+		box-shadow: inset 0 0 6px rgba(197, 119, 119, 0.3);
+	}
 
-    :global(.svelte-tabs li.svelte-tabs__selected:focus){
-        outline: 2px solid transparent;
-        outline-offset: 2px;
-    }
-    :global(.svelte-tabs li.svelte-tabs__selected:active){
-        outline: 2px solid transparent;
-        outline-offset: 2px;
-    }
+	div::-webkit-scrollbar-thumb {
+		background-color: darkgrey;
+		outline: 1px solid slategrey;
+	}
+	@media only screen and (max-height: 550px) {
+		.unlimited {
+			height: fit-content;
+			width: fit-content;
+		}
+	}
+	@media only screen and (max-width: 600px) {
+		.unlimited {
+			height: fit-content;
+			width: fit-content;
+		}
+	}
+	:global(.svelte-tabs li.svelte-tabs__selected) {
+		border-bottom: 4px solid #00917c;
+		color: #00917c;
+		font-weight: 800;
+	}
 
-    :global(.svelte-tabs__tab-list){
-        width: 100% !important;
-        border-bottom: 0px solid #423 !important;
-        align-items: center !important;
-        display: flex;
-    }
-    :global(.svelte-tabs__tab-list li){
-        width: 50%;
-        align-items: center !important;
-    }
+	:global(.svelte-tabs li.svelte-tabs__selected:focus) {
+		outline: 2px solid transparent;
+		outline-offset: 2px;
+	}
+	:global(.svelte-tabs li.svelte-tabs__selected:active) {
+		outline: 2px solid transparent;
+		outline-offset: 2px;
+	}
+
+	:global(.svelte-tabs__tab-list) {
+		width: 100% !important;
+		border-bottom: 0px solid #423 !important;
+		align-items: center !important;
+		display: flex;
+	}
+	:global(.svelte-tabs__tab-list li) {
+		width: 50%;
+		align-items: center !important;
+	}
 </style>
