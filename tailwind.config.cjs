e9e15396173b7b6e7682c40a4fcec42a5f5d413c
veritem <mugaboverite@gmail.com>
2021-05-04 11:21:29 +0200

    chore: added minimal landing page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,8 +3,8 @@ module.exports = {
 	purge: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
-			backgroundImage: theme => ({
-				'landing': "url('landing.jpg')"
+			backgroundImage: (theme) => ({
+				landing: "url('landing.jpg')"
 			})
 		},
 		fontFamily: {
