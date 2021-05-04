a94eee85ac97bc01dec2967b008a22eeb4bb05fa
didiermunezero <didiermunezer38@gmail.com>
2021-05-04 14:40:35 +0900

    fix: undefined error

--- src/routes/$error.svelte
+++ src/routes/$error.svelte
@@ -8,10 +8,18 @@
 	<title>{status}</title>
 </svelte:head>
 
+{#if error}
 <h1>{status}</h1>
 
-<p>{error.message}</p>
+<p>{error?.message}</p>
 
 {#if dev && error.stack}
 	<pre>{error.stack}</pre>
 {/if}
+{:else}
+<div class="w-full flex flex flex-col items-center mx-auto md:mt-20">
+<h1 class="text-center font-bold text-5xl hover:text-green-500 cursor-pointer">404</h1>
+<p class="text-center hover:underline mt-2">Not found</p>
+</div>
+{/if}
+
