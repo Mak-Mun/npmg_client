80155ba4c6fa318b68fe1042953b3182cdb7d828
veritem <mugaboverite@gmail.com>
2021-05-11 21:55:19 +0200

    feat: setting up

--- layout/AuthLayout.tsx
+++ layout/AuthLayout.tsx
@@ -1,17 +1,25 @@
+import Head from 'next/head'
 import Link from 'next/link'
-import React from 'react'
+import React, { Fragment } from 'react'
 
 function AuthLayout({ children }) {
   return (
-    <section className="bg-primaryWhite h-screen  w-screen flex  justify-center items-center flex-col">
-      <div className="font-bold text-9xl block font-sourceSans text-motherGreen text-center mb-10">
-        <Link href="/">
-          <a>NPMG</a>
-        </Link>
-      </div>
-
-      {children}
-    </section>
+    <Fragment>
+      <Head>
+        <title>Siginup</title>
+      </Head>
+      <section
+        style={{ flexDirection: 'column' }}
+        className="flex flex-row bg-primaryWhite h-screen  w-screen   justify-center items-center "
+      >
+        <h1 className="font-bold text-9xl block font-sourceSans text-motherGreen text-center mb-10">
+          <Link href="/">
+            <a>NPMG</a>
+          </Link>
+        </h1>
+        {children}
+      </section>
+    </Fragment>
   )
 }
 
