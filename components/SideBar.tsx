4f71d5eec8a3f855c178b43ba28662f50187947f
didiermunezero <didiermunezer38@gmail.com>
2021-05-12 21:53:02 +0200

    fix: sidebar items and flowing

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -3,15 +3,16 @@ import Link from 'next/link'
 export default function Sidebar({items}){
     const router = useRouter();
     return(
-        <div className="flex bg-white h-screen  shadow-md flex-col">
-	<h2 className="text-3xl uppercase font-sans font-extrabold px-4 mt-16 mb-16">NPMG</h2>
-	<div className="flex flex-col py-4">
-    {items.map((item) => (
+        <div className="flex bg-white h-screen  shadow-md flex-col justify-between">
+			<div>
+	       <h2 className="text-3xl uppercase font-sans font-extrabold px-4 mb-10 mt-10">NPMG</h2>
+	        <div className="flex flex-col py-4">
+            {items.map((item) => (
             item.url == router.pathname ?(
                 <div key={item.url}>
 					<Link href={item.url}>
 					<a
-						className="px-4 flex flex-row items-center h-12 transform  bg-gray-200 border-r-4 border-green-600 font-semibold"
+						className="px-4 flex flex-row items-center h-10 transform  bg-gray-200 border-r-4 border-green-600 font-semibold"
 					>
 						{item.name}
 					</a>
@@ -21,7 +22,7 @@ export default function Sidebar({items}){
                 <div key={item.url}>
 					<Link href={item.url}>
 					<a
-						className="px-4 flex flex-row items-center h-12 transform  text-gray-800 hover:text-motherGreen"
+						className="px-4 flex flex-row items-center h-10 transform  text-gray-800 hover:text-motherGreen"
 					>
 						{item.name}
 					</a>
@@ -29,15 +30,17 @@ export default function Sidebar({items}){
 				</div>
             )
             ))}
-		<div>
+			</div>
+		</div>
+		<div className="mb-3">
 		<Link href="/signin">
 			<a
 				className="text-red-500 font-semibold px-4 py-4 text-center bottom-0 cursor-pointer font-sourceSans"
 				>Logout</a
 			>
-			</Link>
+		</Link>
 		</div>
-	</div>
+	
 </div>
     )
 }
