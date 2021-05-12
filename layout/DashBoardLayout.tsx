564e706c29cb014cbaa52badb6fc2c9e70b28ea4
didiermunezero <didiermunezer38@gmail.com>
2021-05-12 22:10:17 +0200

    feat: fit center of body by outside components

--- layout/DashBoardLayout.tsx
+++ layout/DashBoardLayout.tsx
@@ -41,14 +41,16 @@ export default function DashBoardLayout({children}) {
 		}
 	];
   return (
-    <div className="h-screen bg-gray-100 grid grid-cols-8 gap-0">
+    <div className="h-screen bg-gray-100 grid grid-cols-8 gap-0 overflow-y-hidden">
 		<div className="col-span-1">
 		<SideBar items={list} />
 		</div>
 
-	<div className="col-span-7 flex flex-col">
+	<div className="col-span-7 flex flex-col h-screen overflow-y-auto">
 		<Nav />
+		<div className="px-2 py-2">
 		{children}
+		</div>
 	</div>
 </div>
   )
